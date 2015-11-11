e3218d822632ab6645000aff8956908e2a4eac36
ele0 <ele0@users.noreply.github.com>
2015-11-11 16:06:19 -0500

    Feat: Create header content
     - Removed unnecessary placeholder text
     - Added content for the header within the resumerBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,31 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/* Javascript for Resume Project */
+
+var bio = {
+	"name": "Ele O",
+	"role": "Web Developer",
+	"email": "eleocholi@gmail.com",
+	"github": "https://github.com/ele0",
+	"pictureUrl": "http://web.mit.edu/cron/group/house_n/images/ele150.jpg",
+	"message" : "Hi! Welcome and thanks for checking out my resume!",
+	"skills" : ["Javascript", "HTML with CSS", "Project Management"]
+}
+
+//Format most of the bio elements
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedEmail = HTMLemail.replace("%data%", bio.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.github);
+
+var formattedPic = HTMLbioPic.replace("%data%", bio.pictureUrl);
+var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.message);
+
+//Add the header elements to the page
+$("#header").prepend(formattedName,formattedRole);
+$("#topContacts").append(formattedEmail, formattedGithub);
+$("#header").append(formattedPic, formattedWelcomeMessage, HTMLskillsStart);
+
+//Format and add in each skill
+bio.skills.forEach(function (skill,index,array){
+	var fSkill = HTMLskills.replace("%data%", skill);
+	$("#skills-h3").append(fSkill);
+});
